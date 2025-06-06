69f3f50157b12ae7720b5b671d7091a2b348199b
Ajay Patil <f20230705@goa.bits-pilani.ac.in>
2025-06-06 10:12:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+var typed = new Typed(".auto-type", {
+            strings : [" am ajay patil", "am a frontend developer","enjoy coding !", "watch anime !"],
+            typeSpeed : 150,
+            backSpeed : 150,
+            loop : true
+        })
